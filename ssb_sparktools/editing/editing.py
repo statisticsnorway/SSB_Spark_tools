e2081fb00f361fbc45ba1fa7c45a0c9a18d5ef19
Ane Seierstad <sei@ssb.no>
2020-09-01 12:07:24 +0000

    Docstring changes and removal of logging functions

--- ssb_sparktools/editing/editing.py
+++ ssb_sparktools/editing/editing.py
@@ -10,14 +10,14 @@ import pandas as pd
 from collections import OrderedDict
 import numbers
 from pyspark.sql import DataFrame
-from functools import reduce
+#from functools import reduce
 
 def listcode_lookup(df, luvar, kodeliste, nokkelverdi):
     '''
     
     This function adds a new variable to a given dataframe. The added variable contains values 
     that correspond to the values in a variable in the original dataset. For example; if the
-    the variable on the original dataset is the municipality code, then the function can add
+    variable on the original dataset is the municipality code, then the function can add
     the name of the municipality corresponding to the municipality code.
      
     :param df: The dataframe containing the variable of interest
@@ -77,18 +77,19 @@ def listcode_lookup(df, luvar, kodeliste, nokkelverdi):
 def missing_correction_bool(df, correction_value=False, exception_for=[], df_name=''):
     '''
     
-    This function checks a dataframe for missing values on Boolean variables and corrects the missing values to the given 
-    value given by the correction_value parameter. If no value is given it defaults to False.
+    This function checks a dataframe for missing values on Boolean variables and replaces missing values with the 
+    correction_value parameter. Correction_value defaults to False if no value is given.
     The function corrects all Boolean variables except those specified in the exception_for parameter. 
+    All changes are logged and returned.
    
-    :param df: The dataframe for which to run the missing correction
-    :param correction_value: which value to insert instead of missing
+    :param df: The dataframe to be corrected
+    :param correction_value: value to replace missing
     :param exception_for: list of variables not to be corrected 
-    :type df: dataframe
+    :type df: Spark dataframe
     :type correction_value: Boolean 
     :type exception_for: list
     
-    Returns: 2 dataframes
+    Returns: corrected dataframe and log in that order
     Dataframe: corrected dataframe (Spark) 
     Dataframe: dataframe (Pandas) with log of number of corrections per variable
     
@@ -151,21 +152,22 @@ def missing_correction_bool(df, correction_value=False, exception_for=[], df_nam
                                     
 def missing_correction_number(df, correction_value=0, exception_for=[], df_name=''):
     '''
-    
-    This function checks a dataframe for missing values on numeric variables and corrects the missing values to the given 
-    value given by the correction_value parameter. If no value is given it defaults to 0.
-    Function corrects all numeric variables except those specified in the exception_for-parameter. 
-
-    :param df: The dataframe for which to run the missing correction
-    :param correction_value: which value to insert instead of missing
+    This function checks a dataframe for missing values on numeric variables and replaces missing values with the 
+    correction_value parameter. Correction_value defaults to zero if no value is given.
+    The function corrects all numeric variables except those specified in the exception_for parameter. 
+    All changes are logged and returned.
+   
+    :param df: The dataframe to be corrected
+    :param correction_value: value to replace missing
     :param exception_for: list of variables not to be corrected 
-    :type df: dataframe
-    :type correction_value: numeric value 
+    :type df: Spark dataframe
+    :type correction_value: numeric 
     :type exception_for: list
     
-    Returns: 2 dataframes
+    Returns: corrected dataframe and log in that order
     Dataframe: corrected dataframe (Spark) 
     Dataframe: dataframe (Pandas) with log of number of corrections per variable
+    
     
     '''
     
@@ -225,27 +227,118 @@ def missing_correction_number(df, correction_value=0, exception_for=[], df_name=
             raise Exception('Parameter exception_for må være liste format')
             return
 
+def spark_missing_correction_bool(df, correction_value=False, exception_for=[], df_name=''):
+    #Parameter df --> datasett som det skal kjøres opptelling av missing for
+    #Paramater correction_value --> hvilken verdi som skal settes inn istedenfor missing
+    #Parameter exception_for --> liste over variable som ikke skal korrigeres
+  
+    '''
+    This function checks a dataframe for missing values on Boolean variables and replaces missing values with the 
+    correction_value parameter. Correction_value defaults to False if no value is given.
+    The function corrects all Boolean variables except those specified in the exception_for parameter. 
+    All changes are logged and returned.
+   
+    :param df: The dataframe to be corrected
+    :param correction_value: value to replace missing
+    :param exception_for: list of variables not to be corrected 
+    :type df: Spark dataframe
+    :type correction_value: Boolean 
+    :type exception_for: list
+    
+    Returns: corrected dataframe and log in that order
+    Dataframe: corrected dataframe (Spark) 
+    Dataframe: dataframe (Spark) with log of number of corrections per variable
+    
+    
+    '''  
+    if (isinstance(df, DataFrame)) & (isinstance(correction_value, bool)) & (isinstance(exception_for, type([]))):
+        #initialiserer variabler
+        sc = SparkContext.getOrCreate()
+        sqlContext = SQLContext(sc)
+        boollist = []
+
+        #Legger alle boolske variabler i en egen liste
+        for k in df.schema.fields:
+            if k.name not in exception_for:
+                if (str(k.dataType) == 'BooleanType'):
+                    boollist.append(k.name)
+        #Lager en logg 
+        df_count = df[boollist].select([F.count(F.when(F.isnull(c), c)).alias(c) for c in df[boollist].columns])
+        df_columns = df_count.columns
+        df_log = []
+        
+        if len(df_name)>0:
+            missing_variabler = [StructField('dataframe', StringType(), False),\
+                   StructField('variabel', StringType(), True),\
+                   StructField('datatype', StringType(), True),\
+                   StructField('korrigeringer', IntegerType(), False)]
+            missing_schema = StructType(missing_variabler)
+            
+
+            for row in df_count.rdd.collect():
+                for k in df_columns:
+                    if row[k]!=0:
+                        df_dict_count= {}
+                        df_dict_count['dataframe'] = df_name
+                        df_dict_count['variabel'] = k
+                        df_dict_count['datatype'] = 'boolean'
+                        df_dict_count['korrigeringer'] = row[k]
+                        df_log.append(df_dict_count)
+        else:
+            missing_variabler = [StructField('variabel', StringType(), True),\
+                   StructField('korrigeringer', IntegerType(), False)]
+            missing_schema = StructType(missing_variabler)
+            
+            for row in df_count.rdd.collect():
+                for k in df_columns:
+                    if row[k]!=0:
+                        df_dict_count= {}
+                        df_dict_count['variabel'] = k
+                        df_dict_count['datatype'] = 'boolean'
+                        df_dict_count['korrigeringer'] = row[k]
+                        df_log.append(df_dict_count)
+        
+        if len(df_log)>0:
+            rdd_missing = sc.parallelize(df_log)
+            df_corrections = sqlContext.createDataFrame(rdd_missing, missing_schema)
+        else:
+            df_corrections = sqlContext.createDataFrame(sc.emptyRDD(), missing_schema)
+            
+        #Korrigerer verdier som er boolske
+        df = df.fillna(correction_value, subset=boollist)
+
+            #Returnerer korrigert dataframe (spark) og dictionary med log over antall korrigeringer per variabel 
+        return df, df_corrections
+        
+    else:
+        if not (isinstance(df, DataFrame)):
+            raise Exception('Parameter df må være en dataframe')
+            return
+        if not (isinstance(correction_value, numbers.Number)):
+            raise Exception('Parameter correction_value må være boolsk format')
+            return
+        if not (isinstance(exception_for, type([]))):
+            raise Exception('Parameter exception_for må være liste format')
+            return
+        
 def spark_missing_correction_number(df, correction_value=0, exception_for=[], df_name=''):
     '''
     
-    This function checks a dataframe for missing values on numeric variables and corrects the missing values to the given 
-    value given by the correction_value parameter. If no value is given it defaults to 0.
-    Function corrects all numeric variables except those specified in the exception_for-parameter. 
-    If a column is needed to specify the name of the dataframe it can be given in the df_name-parameter. Useful if checking multiple 
-    dataframes and collecting all logging data in one dataframe outside the function
-
-    :param df: The dataframe for which to run the missing correction
-    :param correction_value: which value to insert instead of missing
+    This function checks a dataframe for missing values on numeric variables and replaces missing values with the 
+    correction_value parameter. Correction_value defaults to zero if no value is given.
+    The function corrects all numeric variables except those specified in the exception_for parameter. 
+    All changes are logged and returned.
+   
+    :param df: The dataframe to be corrected
+    :param correction_value: value to replace missing
     :param exception_for: list of variables not to be corrected 
-    :param df_name: name of the original dataset
-    :type df: dataframe
-    :type correction_value: numeric value 
+    :type df: Spark dataframe
+    :type correction_value: numeric 
     :type exception_for: list
-    :type df_name: string 
     
-    Returns: 2 dataframes
-    Dataframe: Returns corrected dataframe (Spark) 
-    Dataframe: dataframe (Pandas) with log of number of corrections per variable
+    Returns: corrected dataframe and log in that order
+    Dataframe: corrected dataframe (Spark) 
+    Dataframe: dataframe (Spark) with log of number of corrections per variable
     
     '''
     
@@ -322,99 +415,3 @@ def spark_missing_correction_number(df, correction_value=0, exception_for=[], df
             raise Exception('Parameter exception_for må være liste format')
             return
 
-def spark_missing_correction_bool(df, correction_value=False, exception_for=[], df_name=''):
-    #Parameter df --> datasett som det skal kjøres opptelling av missing for
-    #Paramater correction_value --> hvilken verdi som skal settes inn istedenfor missing
-    #Parameter exception_for --> liste over variable som ikke skal korrigeres
-  
-    '''
-    
-    This function checks a dataframe for missing values on Boolean variables and corrects the missing values to the given 
-    value given by the correction_value parameter. If no value is given it defaults to False.
-    The function corrects all Boolean variables except those specified in the exception_for parameter. 
-    If a column is needed to specify the name of the dataframe it can be given in the df_name parameter. Useful if checking multiple 
-    dataframes and collecting all logging data in one dataframe outside the function
-   
-    :param df: The dataframe for which to run the missing correction
-    :param correction_value: which value to insert instead of missing
-    :param exception_for: list of variables not to be corrected 
-    :param df_name: name of the original dataset
-    :type df: dataframe
-    :type correction_value: Boolean 
-    :type exception_for: list
-    :type df_name: string 
-    
-    Returns: 2 dataframes
-    Dataframe: corrected dataframe (Spark) 
-    Dataframe: dataframe (Pandas) with log of number of corrections per variable
-    
-    '''  
-    if (isinstance(df, DataFrame)) & (isinstance(correction_value, bool)) & (isinstance(exception_for, type([]))):
-        #initialiserer variabler
-        sc = SparkContext.getOrCreate()
-        sqlContext = SQLContext(sc)
-        boollist = []
-
-        #Legger alle boolske variabler i en egen liste
-        for k in df.schema.fields:
-            if k.name not in exception_for:
-                if (str(k.dataType) == 'BooleanType'):
-                    boollist.append(k.name)
-        #Lager en logg 
-        df_count = df[boollist].select([F.count(F.when(F.isnull(c), c)).alias(c) for c in df[boollist].columns])
-        df_columns = df_count.columns
-        df_log = []
-        
-        if len(df_name)>0:
-            missing_variabler = [StructField('dataframe', StringType(), False),\
-                   StructField('variabel', StringType(), True),\
-                   StructField('datatype', StringType(), True),\
-                   StructField('korrigeringer', IntegerType(), False)]
-            missing_schema = StructType(missing_variabler)
-            
-
-            for row in df_count.rdd.collect():
-                for k in df_columns:
-                    if row[k]!=0:
-                        df_dict_count= {}
-                        df_dict_count['dataframe'] = df_name
-                        df_dict_count['variabel'] = k
-                        df_dict_count['datatype'] = 'boolean'
-                        df_dict_count['korrigeringer'] = row[k]
-                        df_log.append(df_dict_count)
-        else:
-            missing_variabler = [StructField('variabel', StringType(), True),\
-                   StructField('korrigeringer', IntegerType(), False)]
-            missing_schema = StructType(missing_variabler)
-            
-            for row in df_count.rdd.collect():
-                for k in df_columns:
-                    if row[k]!=0:
-                        df_dict_count= {}
-                        df_dict_count['variabel'] = k
-                        df_dict_count['datatype'] = 'boolean'
-                        df_dict_count['korrigeringer'] = row[k]
-                        df_log.append(df_dict_count)
-        
-        if len(df_log)>0:
-            rdd_missing = sc.parallelize(df_log)
-            df_corrections = sqlContext.createDataFrame(rdd_missing, missing_schema)
-        else:
-            df_corrections = sqlContext.createDataFrame(sc.emptyRDD(), missing_schema)
-            
-        #Korrigerer verdier som er boolske
-        df = df.fillna(correction_value, subset=boollist)
-
-            #Returnerer korrigert dataframe (spark) og dictionary med log over antall korrigeringer per variabel 
-        return df, df_corrections
-        
-    else:
-        if not (isinstance(df, DataFrame)):
-            raise Exception('Parameter df må være en dataframe')
-            return
-        if not (isinstance(correction_value, numbers.Number)):
-            raise Exception('Parameter correction_value må være boolsk format')
-            return
-        if not (isinstance(exception_for, type([]))):
-            raise Exception('Parameter exception_for må være liste format')
-            return
--- ssb_sparktools/processing/processing.py
+++ ssb_sparktools/processing/processing.py
@@ -1,15 +1,46 @@
 import pyspark.sql.functions as F
 from pyspark.sql.types import *
+
+def cross_sectional(df, event_var, event_id, coDate=None):
+    '''
+    This function makes a cross sectional dataset of the last record before a defined date
+    for all uniquely identified units in a dataframe (df).
+    The date for which the cross sectional dataset is made is defined in the parameter coDate; 
+    if noe date is given, the default date is today.
+    The records unique id is defined in the parameter event_id. 
+    The time at which the event happens is given in the variable event_var.
     
+    :param df: The dataframe of observations from which to make a cross sectional dataset 
+    :type df: dataframe 
+    :param event_var: a variable containing the date at which the event happened
+    :type event_var:  time/date 
+    :param event_id: the variables that uniquely define a record
+    :type event_id: list
+    :param coDate: the date at which the cross sectional dataset is taken.
+    :type coDate: time/date 
+    
+    Returns:a dataframe
+    Dataframe: A cross sectional dataframe.
+    '''
+    if coDate!=None:
+        df = df.filter(F.col(event_var) <= coDate)
+    
+    eventlist_ids = event_id.copy()
+    eventlist_ids.append(event_var)
+    
+    df_cross = df.join(df.groupBy(event_id).agg(F.max(event_var).alias(event_var)),\
+                          eventlist_ids, how='inner')
+    return df_cross
+
 def traverse_hierarchy(keylist, travdf, parqdf, idstreng, hierarchylevels):
     '''
     This function walks through a hierarcical dataset stored in memory,
-    and returns all packed as unpacked data objects.
+    and unpacks all packed data objects.
     
-    The function takes a hirarchical dataset, investigates the schema structre 
+    The function takes a hirarchical dataset, investigates the schema structure 
     to find any other hirarchy elements of type struct, list, and arrays. 
-    All objects found in the data object are subsequently unpacked, 
-    and returend as a data object. 
+    All objects found in these data objects are subsequently unpacked, 
+    and replaces the packed data objects. 
     
     This is a recursive function, which ends when there are no list or array type 
     columns left in the original data object. 
@@ -19,13 +50,13 @@ def traverse_hierarchy(keylist, travdf, parqdf, idstreng, hierarchylevels):
     
     :param keylist: List containing the variables you want to carry forward from the level above, to the level below
     :type keylist: list
-    :param travdf: 
-    :type travdf: 
+    :param travdf: data object to unpack
+    :type travdf: string
     :param parqdf: The hierarchical dataset that you are investigating 
     :type parqdf: dataframe
-    :param idstreng: 
-    :type idstreng: 
-    :param hierarchylevels: controls the depth at which the funcrion will look for arrays 
+    :param idstreng: identifying record in parent dataframe
+    :type idstreng: string
+    :param hierarchylevels: controls the depth at which the function will look for arrays 
     :type hierarchylevels: numeric value
     
      Returns: 
@@ -99,20 +130,22 @@ def traverse_hierarchy(keylist, travdf, parqdf, idstreng, hierarchylevels):
 def unpack_parquet(parqdf, rootdf=False, rootvar=True, levels=-1):
     
     '''
-    This function unpacks a hierarchical parquet dataframe.
+    This function unpacks a hierarchical spark dataframe and relies on function traverse_hierarchy to traverse the hierarchy and unpack.
+    Each unpacked object is a dataframe that gets stored in a dictionary which is then returned to the user.
     
     The parameter rootdf is used to decide whether to create a separate root level dataset without the variables to 
     be extracted. 
         Usage: 
         True: a root level dataset is made. 
         False: a root level dataset is not made. 
-        List: a dataset with root level variables defined in a list. The default value is False.
+        List: a dataset with root level variables defined in a list. 
+        The default value is False.
 
     The parameter rootvar is used to tell how the variables at the root level are to be treated.
         Usage: 
-        True: all variables, which do not need to be unpacked, at the root level, will also be included in all data sets that are unpacked.
-        False: root-level variables will only be included in the root dataset if rootdf is set to True.
-        List: List of variables to include from root level to the extracted datasets
+        True: all variables, which do not need to be unpacked, at the root level, will also be included in all datasets that are unpacked.
+        False: root level variables will not be included in the datasets that are unpacked.
+        List: list of variables from root level to be added to the datasets that are unpacked. 
     
     The levels parameter tells the function how many hierarchy levels it will traverse and unpack. 
         Usage:
@@ -121,13 +154,13 @@ def unpack_parquet(parqdf, rootdf=False, rootvar=True, levels=-1):
         1: the function unpacks one level  
         2: the function unpacks two levels etc.
     
-    :param parqdf: The hierarchical Parquet dataframe to be unpacked.
+    :param parqdf: The dataframe with hierarchical structure to be unpacked.
     :type parqdf: dataframe 
-    :param rootdf: Decides whether to create a separate root level dataset without the variables to be extracted.
-    :type rootdf: Boolean/list 
-    :rootvar: tells how the variables at the root level are to be treated.
-    :type rootvar: Boolean/list
-    :param levels: tells the function how many hierarchy levels the dataset consists of
+    :param rootdf: Decides whether to create a separate root level dataset.
+    :type rootdf: boolean/list 
+    :rootvar: Tells if the root level variables are to be transfered to the unpacked objects or not.
+    :type rootvar: boolean/list
+    :param levels: Tells the function how many hierarchy levels to be traversed
     :type levels: numerical value
     
      Returns: a dictionary
@@ -178,33 +211,3 @@ def unpack_parquet(parqdf, rootdf=False, rootvar=True, levels=-1):
             
     return ds_dict.copy()
         
-def cross_sectional(df, event_var, event_id, coDate=None):
-    '''
-    This function makes a cross sectional dataset of the last record before a defined date
-    for all uniquely identified units in a dataframe (df).
-    The date for which the cross sectional dataset is made is defined in the parameter coDate; 
-    if noe date is given, the default date is today.
-    The records unique id is defined in the parameter event_id. 
-    The time at which the event happens is given in the variable event_var.
-    
-    :param df: The dataframe of observations from which to make a cross sectional dataset 
-    :type df: dataframe 
-    :param event_var: a variable containing the date at which the event happened
-    :type event_var:  time/date 
-    :param event_id: the variables that uniquely define a record
-    :type event_id: list
-    :param coDate: the date at which the cross sectional dataset is taken.
-    :type coDate: time/date 
-    
-    Returns:a dataframe
-    Dataframe: A cross sectional dataframe.
-    '''
-    if coDate!=None:
-        df = df.filter(F.col(event_var) <= coDate)
-    
-    eventlist_ids = event_id.copy()
-    eventlist_ids.append(event_var)
-    
-    df_cross = df.join(df.groupBy(event_id).agg(F.max(event_var).alias(event_var)),\
-                          eventlist_ids, how='inner')
-    return df_cross
