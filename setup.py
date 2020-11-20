b693adad94c462dedfe5b90e0820ca0456689711
Simen Svenkerud <simen.svenkerud@ssb.no>
2020-11-20 13:15:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ DEPENDENCIES = [
 
 setup(
     name="ssb_spark_tools",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/statisticsnorway/ssb_spark_tools",
     license='MIT',
 
